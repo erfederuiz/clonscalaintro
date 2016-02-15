468bf26d6a09348e354f7fed8e0a41b31d75c7be
Jesús López-González <jesus@kathars.es>
2016-02-15 17:26:44 +0100

    Added some corrections

--- src/main/scala/org/hablapps/curso/azucar/lista.scala
+++ src/main/scala/org/hablapps/curso/azucar/lista.scala
@@ -3,46 +3,46 @@ package org.hablapps.curso.azucar
 // 0. Partimos de la lista del módulo 3, incluyendo los ejercicios en notación
 // infija.
 
-sealed trait Lista {
-
-  def insertar(elemento: Int): Lista = {
-    new Cons(elemento, this)
-  }
-
-  def suma: Int = this match {
-    case Cons(cabeza, resto) => cabeza + resto.suma
-    case Nada() => 0
-  }
-
-  def map(f: Int => Int): Lista = this match {
-    case Cons(cabeza, resto) => Cons(f(cabeza), resto.map(f))
-    case Nada() => Nada()
-  }
-
-  def concatenar(l: Lista): Lista = this match {
-    case Cons(cabeza, resto) => Cons(cabeza, resto.concatenar(l))
-    case Nada() => l
-  }
-
-  def existe(f: Int => Boolean): Boolean = this match {
-    case Cons(cabeza, _) if f(cabeza) => true
-    case Cons(_, resto) => resto.existe(f)
-    case Nada() => false
-  }
-
-  def contiene(l: Lista, i: Int): Boolean = {
-    existe(j => i == j)
-  }
-
-  def dropWhile(f: Int => Boolean): Lista = this match {
-    case Cons(cabeza, resto) if f(cabeza) => resto.dropWhile(f)
-    case _ => this
-  }
-}
-
-case class Cons(cabeza: Int, resto: Lista) extends Lista
-
-case class Nada() extends Lista
+// sealed trait Lista {
+//
+//   def insertar(elemento: Int): Lista = {
+//     new Cons(elemento, this)
+//   }
+//
+//   def suma: Int = this match {
+//     case Cons(cabeza, resto) => cabeza + resto.suma
+//     case Nada() => 0
+//   }
+//
+//   def map(f: Int => Int): Lista = this match {
+//     case Cons(cabeza, resto) => Cons(f(cabeza), resto.map(f))
+//     case Nada() => Nada()
+//   }
+//
+//   def ++(l: Lista): Lista = this match {
+//     case Cons(cabeza, resto) => Cons(cabeza, resto.++(l))
+//     case Nada() => l
+//   }
+//
+//   def existe(f: Int => Boolean): Boolean = this match {
+//     case Cons(cabeza, _) if f(cabeza) => true
+//     case Cons(_, resto) => resto.existe(f)
+//     case Nada() => false
+//   }
+//
+//   def contiene(l: Lista, i: Int): Boolean = {
+//     existe(j => i == j)
+//   }
+//
+//   def dropWhile(f: Int => Boolean): Lista = this match {
+//     case Cons(cabeza, resto) if f(cabeza) => resto.dropWhile(f)
+//     case _ => this
+//   }
+// }
+//
+// case class Cons(cabeza: Int, resto: Lista) extends Lista
+//
+// case class Nada() extends Lista
 
 // 1. Aplicación de parámetros por defecto en el constructor de `Cons`.
 
--- src/main/scala/org/hablapps/curso/funcional/Ejercicios.scala
+++ src/main/scala/org/hablapps/curso/funcional/Ejercicios.scala
@@ -2,9 +2,13 @@ package org.hablapps.curso.funcional
 
 object Ejercicios extends App {
 
-  // Ejercicio 1: Implementa `concatenar`
+  // Ejercicio 1: Implementa `concatenar` (utiliza la siguiente signatura)
 
-  // Ejercicio 2: Implementa `existe`
+  // def concatenar(l1: Lista, l2: Lista): Lista
+
+  // Ejercicio 2: Implementa `existe`.
+  // - El tipo Boolean contiene los valores `true` y `false`
+  // - El operador lógico "or" se representa mediante `b1 || b2`.
 
   // Ejercicio 3: Implementa el método `contiene` que se apoya en `existe`
 
@@ -12,4 +16,6 @@ object Ejercicios extends App {
   // elementos de la cabeza que cumplan con la condición, hasta que encuentre
   // uno que no lo haga, momento en el que se devolverá el resto de la lista
   // (elemento incluido)
+  // - La estructura de control de flujo "if" se despliega con esta sintáxis:
+  // `if(b) expr1 else expr2`
 }
