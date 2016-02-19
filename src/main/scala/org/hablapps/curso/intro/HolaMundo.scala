ec3fcda11a0d2d9422aa70dfe4485a45a20428d2
Jesús López-González <jesus@kathars.es>
2016-02-19 16:25:03 +0100

    Module 1

--- src/main/scala/org/hablapps/curso/intro/HolaMundo.scala
+++ src/main/scala/org/hablapps/curso/intro/HolaMundo.scala
@@ -2,4 +2,14 @@ package org.hablapps.curso.intro
 
 // 1. Implementación dulcificada del Hola Mundo!
 
+object HolaMundo extends App {
+  println("Hola Mundo!")
+}
+
 // 2. Implementación clásica del Hola Mundo!
+
+object HolaMundo2 {
+  def main(args: Array[String]): Unit = {
+    println("Hola Mundo 2!")
+  }
+}
